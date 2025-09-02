4195b1f8b2770034281dbcd345894028efde17d8
Jean-Philippe Braun <jean-philippe.braun@gandi.net>
2025-09-02 11:52:33 +0200

    Add some pgsql code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
+import psycopg
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    with psycopg.connect(dbname="postgres", user="hosting-db", host="localhost") as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS test (
+                    id serial PRIMARY KEY,
+                    data text)
+                """)
+            cur.execute("INSERT INTO test (data) VALUES (%s)", ("Hello, World!",))
+            conn.commit()
+
+            cur.execute("SELECT * FROM test")
+            _, data = cur.fetchone()
+            return f"<p>{data.decode('UTF-8')}</p>"
